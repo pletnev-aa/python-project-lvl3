cf7e3960f9448de5b34583d180dcc834df7f6967
pletnev-aa <apletnev@list.ru>
2021-07-07 15:19:44 +0400

    fixed: add check download and save image, and check changes html

--- tests/test_pageload.py
+++ tests/test_pageload.py
@@ -6,20 +6,47 @@ from pathlib import Path
 from pageload import parse, fs
 
 
-@pytest.mark.parametrize('url,html', [
+@pytest.mark.parametrize('url,html,img,exp_html', [
     ('https://page-loader.hexlet.repl.co',
      'tests/fixtures/page-loader-hexlet-repl-co.html',
+     'tests/fixtures/page-loader-hexlet-repl-co-assets-professions-nodejs.png',
+     'tests/fixtures/expected.html'
      ),
 ])
-def test_download_html(url, html):
+def test_download_html(url, html, img, exp_html):
     with open(html) as data:
-        exp_html = data.read()
-    with requests_mock.Mocker() as mock:
-        mock.get(url, text=exp_html)
+        test_html = data.read()
         data.close()
+    with open(img, 'rb') as data:
+        test_img = data.read()
+        data.close()
+    with open(exp_html) as data:
+        exp_html = data.read()
+        data.close()
+    with requests_mock.Mocker() as mock:
+        mock.get(url, text=test_html)
     with tempfile.TemporaryDirectory() as path:
-        ath = Path(path)
+        path = Path(path)
         name = parse.get_name(url) + '.html'
-        result = fs.save_data(ath / name, requests.get(url).text)
-        assert Path.is_file(result)
-        assert Path(result).name == Path(html).name
+        app_html = fs.save_data(
+            path / name,
+            requests.get(url).text
+        )
+        assert Path.is_file(app_html)
+        assert Path(app_html).name == Path(html).name
+        assert Path.read_text(app_html) == test_html
+        domain = parse.get_domain(url + '/courses')
+        app = Path(parse.get_name(url) + '_files')
+        fs.make_dir(path / app)
+        assets, finished_html = parse.get_assets(app_html, domain, app)
+        mock.get(assets[Path(img).name], content=test_img)
+        finished_img = fs.save_data(
+            path / app / Path(img).name,
+            requests.get(assets[Path(img).name]).content
+        )
+        result = fs.save_data(app_html, finished_html)
+        assert domain == url
+        assert Path.is_dir(path / app)
+        assert Path.is_file(finished_img)
+        assert Path.read_bytes(finished_img) == test_img
+        assert Path.read_text(result) == exp_html
